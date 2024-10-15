c07e8b3f7cf0c581eddd757d05ec00cd02eaa135
wongaz <awong593@gmail.com>
2024-10-15 11:01:47 -0700

    Reorganizing matchmaking rules

--- src/main/kotlin/matchsimulation/rules/AlwaysBlueFavoredSim.kt
+++ src/main/kotlin/matchsimulation/rules/AlwaysBlueFavoredSim.kt
@@ -2,9 +2,8 @@ package io.wongaz.matchsimulation.rules
 
 import io.wongaz.matchsimulation.interfaces.IGameSimulation
 import io.wongaz.model.core.Team
-import kotlin.random.Random
 
-class BlueSideFavoredSim(private val seededRandom: Random = Random.Default ) : IGameSimulation {
+class AlwaysBlueFavoredSim(): IGameSimulation {
     override fun runSingleGameSimulation(team1: Team, team2: Team): Team {
         return team1
     }

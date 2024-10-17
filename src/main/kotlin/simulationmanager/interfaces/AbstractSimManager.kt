e3ffd24e537f673e51e22d77f053cdc7c321377a
wongaz <awong593@gmail.com>
2024-10-17 12:59:57 -0700

    coroutine library

--- src/main/kotlin/simulationmanager/interfaces/AbstractSimManager.kt
+++ src/main/kotlin/simulationmanager/interfaces/AbstractSimManager.kt
@@ -4,7 +4,7 @@ import io.wongaz.model.core.Team
 import io.wongaz.model.simulation.SimulationResult
 
 abstract class AbstractSimManager(val teams: List<Team>, protected val iterations: Int) {
-    val simResults = teams.map { it.teamSignature to SimulationResult(it, iterations) }.toMap()
+    private val simResults = teams.map { it.teamSignature to SimulationResult(it, iterations) }.toMap()
 
     abstract fun doWork()
 
